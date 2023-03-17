a7fc6199a5a3a3e4aa47932462015fd0741ef0ea
Hakim <hakimmajid243@gmail.com>
2023-03-17 08:53:44 +0700

    first commit

---
+++ app/Models/Tanggapan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tanggapan extends Model
+{
+    use HasFactory;
+    protected $primaryKey  = 'id_tanggapan';
+    protected $fillable = [
+        'tanggapan',
+        'id_pengaduan',
+        'id_petugas',
+        'tgl_tanggapan',
+    ];
+}
---
+++ database/migrations/2023_03_08_015805_create_pengaduan_procedure.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE PROCEDURE update_pengaduan_status(IN `idx` INT, IN `Statusx` ENUM("0", "Proses", "Selesai"))
+        BEGIN
+            UPDATE `pengaduans` SET `status` = `Statusx` WHERE id_pengaduan = `idx`;
+        END');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduan_procedure');
+    }
+};
